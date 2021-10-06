4e2aa2ad95516144a74f833c3056a5cd47bc31c7
unknown <riksakn00@gmail.com>
2021-10-07 06:39:55 +0700

    Membuat Aplikasi MVC dengan PHP #Controller

---
+++ app/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+/** 
+ * Riksa Kusumah Nagara
+ * 193040078
+ * https://github.com/riksanagara/prakweb2021_phpmvc_193040078
+*/
+
+class Home {
+    public function index()
+    {
+        echo 'Home/index';
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -7,10 +7,42 @@
 */
 
 class App{
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
         $url = $this->parseURL();
-        var_dump($url);
+        
+        //controller
+        if($url == NULL)
+        {
+			$url = [$this->controller];
+		}
+        if(file_exists('../app/controllers/' . $url[0] . '.php')){
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        //method
+        if(isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        //prams
+        if(!empty($url)){
+            $this->params = array_values($url);
+        }
+
+        // run controller & method, and send params if exist
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL()
